019c839a5ce3b9ef98e0b01a49128573ad3e62f3
OMNIA <102552702+Omniasherief@users.noreply.github.com>
2025-12-12 21:02:51 +0200

    update

---
+++ ros2_ws/src/labs/lab8_my_robot_mapping/launch/map_loader.launch.py
@@ -0,0 +1,60 @@
+import os
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+#from launch.actions import TimerAction
+
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    map_name = LaunchConfiguration("map")
+
+
+    lifecycle_nodes = ["map_server"]
+
+    use_sim_time_arg = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="true"
+    )
+
+    map_path = PathJoinSubstitution([
+        get_package_share_directory("lab8_my_robot_mapping"),
+        "maps",
+        "house",
+        "map.yaml"
+    ])
+
+    nav2_map_server = Node(
+        package="nav2_map_server",
+        executable="map_server",
+        name="map_server",
+        output="screen",
+        parameters=[
+            {"yaml_filename": map_path},
+            {"use_sim_time": use_sim_time}
+        ],
+    )
+
+    nav2_lifecycle_manager = Node(
+        package="nav2_lifecycle_manager",
+        executable="lifecycle_manager",
+        name="lifecycle_manager_slam",
+        output="screen",
+        parameters=[
+            {"node_names": lifecycle_nodes},
+            {"use_sim_time": use_sim_time},
+            {"autostart": True}
+        ],
+    )
+
+
+
+
+    return LaunchDescription([
+        use_sim_time_arg,
+        nav2_map_server,
+        nav2_lifecycle_manager,
+    ])
---
+++ ros2_ws/src/labs/lab9_my_robot_bringup/launch/simulated_robot.launch.py
@@ -0,0 +1,73 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable, ExecuteProcess
+
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+#from launch.actions import TimerAction
+
+def generate_launch_description():
+
+    gazebo = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("lab5_my_robot_description"),
+            "launch",
+            "gazebo.launch.py"
+        ),
+    )
+    
+    controller = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("lab6_my_robot_controllers"),
+            "launch",
+            "controller.launch.py"
+        ),
+        launch_arguments={
+            "use_simple_controller": "False",
+            "use_python": "False"
+        }.items(),
+    )
+    
+    teleop = ExecuteProcess(
+        cmd=[
+            'gnome-terminal', '--', 'bash', '-c',
+            'export TURTLEBOT3_MODEL=burger; ros2 run teleop_twist_keyboard teleop_twist_keyboard   --ros-args -r /cmd_vel:=/diff_drive_controller/cmd_vel_unstamped'
+        ],
+        name='teleop_keyboard'
+    )
+
+    slam = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("lab8_my_robot_mapping"),
+            "launch",
+            "slam.launch.py"
+        ),
+    )
+ 
+
+
+    rviz_slam = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=["-d", os.path.join(
+                get_package_share_directory("lab8_my_robot_mapping"),
+                "rviz",
+                "slam.rviz"
+            )
+        ],
+        output="screen",
+        parameters=[{"use_sim_time": True}],
+    )
+    
+   
+    return LaunchDescription([
+        gazebo,
+        controller,
+       
+        slam,
+        rviz_slam,
+        teleop,
+
+    ])
